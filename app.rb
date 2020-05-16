70b88a17a7709f5a225140859b81e102712daae3
Hari Mohanraj <hari.mohanraj89@gmail.com>
2020-05-16 19:38:18 +0530

    Add config.ru

--- app.rb
+++ app.rb
@@ -1,10 +1,8 @@
 require 'slack-ruby-bot'
 require 'dotenv/load'
 
-class Slackbot < SlackRubyBot::Bot
+class App < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
   end
 end
-
-Slackbot.run
